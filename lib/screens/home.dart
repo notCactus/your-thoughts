4d3d2b4c271478f20d0840e1e8c52e70e9b986e2
notCactus <cactusdevcode@gmail.com>
2020-06-28 23:56:28 +0200

    Make messages stored at firebas/firestore

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../widgets/message.dart';
 import '../models/message.dart' as msg;
@@ -10,16 +11,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<msg.Message> _messages = [
-    msg.Message(
-        message:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula blandit sapien bibendum gravida. Suspendisse aliquet molestie nulla. Vestibulum fermentum libero ac turpis faucibus aliquet vel dictum mauris. Dio scelerisque in. Phasellus nisl nunc, posuere vitae scelerisque.',
-        image: 'Image'),
-    msg.Message(
-      message: 'This is a test message.',
-      image: 'Image',
-    ),
-  ];
+  List<msg.Message> _messages = [];
+
+  void silentAddMessage(String text) {
+    if (text != '') {
+      _messages.add(
+        msg.Message(message: text, image: 'Image'),
+      );
+    }
+  }
 
   void addMessage(String text) {
     if (text != '') {
@@ -28,6 +28,7 @@ class _HomeState extends State<Home> {
           msg.Message(message: text, image: 'Image'),
         );
       });
+      Firestore.instance.collection('messages').add({'message': text});
     }
   }
 
@@ -37,15 +38,28 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('YourThoughts'),
       ),
-      body: Container(
-        margin: EdgeInsets.only(bottom: 56),
-        child: ListView.builder(
-          itemCount: _messages.length,
-          itemBuilder: (context, i) => Message(
-            message: _messages[i].message,
-          ),
-        ),
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('messages').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Text('Loading...');
+              default:
+                _messages = snapshot.data.documents
+                    .map(
+                      (d) => msg.Message(message: d['message'], image: 'Image'),
+                    )
+                    .toList();
+                return ListView.builder(
+                  itemCount: _messages.length,
+                  itemBuilder: (context, i) => Message(
+                    message: _messages[i].message,
+                  ),
+                );
+            }
+          }),
       bottomSheet: SendMessage(
         msgFunction: addMessage,
       ),
